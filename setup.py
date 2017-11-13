9c9689a259a0e7626d5a3f0000c1567241b9c463
chrispaulca <cpcsiszar@usfca.edu>
2017-11-13 15:52:30 -0800

    try7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'waterfall_charts',
-  packages = ['waterfall_charts'],
+  name = 'waterfallcharts',
+  packages = ['waterfallcharts'],
   version = '0.1',
   description = 'Quickly generates standard waterfall charts, takes two ordered lists as inputs.',
   author = 'Christopher Paul Csiszar',
