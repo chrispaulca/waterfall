dd29f77e30d9615257440f66b8e9c8960c47abc5
chrispaulca <cpcsiszar@usfca.edu>
2017-11-13 15:44:05 -0800

    try2

---
+++ README.txt
@@ -0,0 +1,3 @@
+!! This is a work in progress package !!
+
+Waterfall_charts attempts to provide an easy and quick way to plot basic (for now) and more advanced (to come) waterfall charts of the accounting and finance type.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'waterfall_chart',
+  packages = ['waterfall_chart'],
+  version = '0.1',
+  description = 'Quickly generates standard waterfall charts, takes two ordered lists as inputs.',
+  author = 'Christopher Paul Csiszar',
+  author_email = 'christopher.csiszar@gmail.com',
+  url = 'https://github.com/chrispaulca/waterfall/tree/master/waterfall_charts', 
+  download_url = 'https://github.com/chrispaulca/waterfall/archive/0.1.tar.gz',
+  keywords = ['waterfall', 'chart', 'charts', 'accounting', 'automate'],
+  classifiers = [],
+)
