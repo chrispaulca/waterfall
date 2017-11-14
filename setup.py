091d660da4171765875d8cd365864fb1140fe10f
chrispaulca <cpcsiszar@usfca.edu>
2017-11-13 16:32:15 -0800

    try

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'waterfallcharts',
   packages = ['waterfallcharts'],
-  version = '0.2',
+  version = '0.3',
   description = 'Quickly generates standard waterfall charts, takes two ordered lists as inputs.',
   author = 'Christopher Paul Csiszar',
   author_email = 'christopher.csiszar@gmail.com',
   url = 'https://github.com/chrispaulca/hosted_waterfall/tree/master', 
-  download_url = 'https://github.com/chrispaulca/hosted_waterfall/archive/0.2.tar.gz',
+  download_url = 'https://github.com/chrispaulca/hosted_waterfall/archive/0.3.tar.gz',
   keywords = ['waterfall', 'chart', 'charts', 'accounting', 'automate'],
   classifiers = [],
 )
