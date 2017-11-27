10b423cc00e2ce3b61a5630c6e56a965c1c91cca
chrispaulca <cpcsiszar@usfca.edu>
2017-11-27 12:04:20 -0800

    restructuring and renaming

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'waterfallcharts',
   packages = ['waterfallcharts'],
-  version = '2.3',
+  version = '2.4',
   description = 'Quickly generates standard waterfall charts, takes two ordered lists as inputs.',
   author = 'Christopher Paul Csiszar',
   author_email = 'christopher.csiszar@gmail.com',
   url = 'https://github.com/chrispaulca/hosted_waterfall/tree/master', 
-  download_url = 'https://github.com/chrispaulca/hosted_waterfall/archive/v2.3.tar.gz',
+  download_url = 'https://github.com/chrispaulca/hosted_waterfall/archive/v2.4.tar.gz',
   keywords = ['waterfall', 'chart', 'charts', 'accounting', 'automate', 'random forest', 'decision tree'],
   classifiers = [],
 )
