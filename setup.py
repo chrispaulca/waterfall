b5d0f1c47bb9462232fdd47ea6524f4bbf48b536
chrispaulca <cpcsiszar@usfca.edu>
2017-11-27 13:09:38 -0800

    v3.4

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
 
 setup(
-  name = 'waterfall',
-<<<<<<< HEAD
-  py_modules=['waterfall'],
-=======
-  packages = ['waterfall'],
->>>>>>> ba04a8d83f247c9e1ee500860478a4a7afe1c2ee
-  version = '3.0',
+  name = 'waterfallcharts',
+  py_modules=['waterfall_chart'],
+  version = '3.4',
   description = 'Quickly generates standard waterfall charts, takes two ordered lists as inputs.',
   author = 'Christopher Paul Csiszar',
   author_email = 'christopher.csiszar@gmail.com',
   url = 'https://github.com/chrispaulca/waterfall', 
-  download_url = 'https://github.com/chrispaulca/waterfall/archive/v3.0.tar.gz',
+  download_url = 'https://github.com/chrispaulca/waterfall/archive/v3.4.tar.gz',
   keywords = ['waterfall', 'chart', 'charts', 'accounting', 'automate', 'random forest', 'decision tree'],
   classifiers = [],
 )
